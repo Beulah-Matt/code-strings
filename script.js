5cf931cf753196d25ac808b5241869bced84b680
Beulah Mathenge <beulahmathenge@gmail.com>
2022-08-12 09:12:17 +0300

    Updated About Section

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
 const h2 = document.createElement("h2");
 h2.textContent = "What makes me tick";
 document.querySelector("body").appendChild(h2);
+
+
+document.getElementById("h2").innerHTML= "I am a developer who first began as a hobbyist before finding my way to Moringa." 
+"I love creating and seeing ideas come to life. When I am not working, I will be chasing mountains and going to an orchestra perfomance near menubar."
+
