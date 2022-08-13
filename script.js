4a80b4e726abb4caf64eb18f694e4963f5b4882c
Beulah Mathenge <beulahmathenge@gmail.com>
2022-08-13 10:13:49 +0300

    Icons and Button event

--- script.js
+++ script.js
@@ -3,6 +3,17 @@ h2.textContent = "What makes me tick";
 document.querySelector("body").appendChild(h2);
 
 
-document.getElementById("h2").innerHTML= "I am a developer who first began as a hobbyist before finding my way to Moringa." 
-"I love creating and seeing ideas come to life. When I am not working, I will be chasing mountains and going to an orchestra perfomance near menubar."
+document.getElementById("h2").innerHTML= "I am a developer who first began as a hobbyist before finding my way to Moringa. I love creating and seeing ideas come to life. When I am not working, I will be chasing mountains and going to an orchestra perfomance near me.";
 
+debugger;
+
+//When button is clicked
+const button= document.getElementsByClassName("play-button")
+
+function addingEventListener() {
+   button.addEventListener('click',clickAlert) 
+}
+function clickAlert(){
+    
+alert('I was clicked');  
+}    
